bd08850a527db30fcd512051616996c3a815b5d5
martin <91955997+mrtnhwtt@users.noreply.github.com>
2024-09-29 06:43:05 +0900

    wip

--- tui/board.go
+++ tui/board.go
@@ -1,11 +1,14 @@
 package tui
 
 import (
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/mrtnhwtt/kittypass/internal/kittypass"
 	"github.com/mrtnhwtt/kittypass/internal/storage"
 	"github.com/spf13/viper"
 )
+
 
 type focus int
 
@@ -26,6 +29,8 @@ const (
 
 type Board struct {
 	focus      focus
+	status     status
+	spinner    spinner.Model
 	vaultList  VaultListModel
 	loginsList LoginsListModel
 	login      LoginDisplayModel
@@ -34,9 +39,15 @@ type Board struct {
 }
 
 func NewBoard(v *viper.Viper) *Board {
+	vl := NewVaultList()
 	board := &Board{
-		conf: v,
+		conf:      v,
+		vaultList: vl,
+		status: Loading,
 	}
+	board.spinner = spinner.New()
+	board.spinner.Spinner = spinner.Line
+	board.spinner.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
 	return board
 }
 
@@ -47,39 +58,50 @@ func (b Board) Init() tea.Cmd {
 func (b Board) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		if msg.String() == "ctrl+c" {
+		switch msg.String() {
+		case "ctrl+c", "q":
 			return b, tea.Quit
 		}
 	case tea.WindowSizeMsg:
-		// h, v := docStyle.GetFrameSize()
-		// b.list.SetSize(msg.Width-h, msg.Height-v)
+		b.vaultList.Update(BlockSizeMsg{Width: msg.Width / 3, Height: msg.Height})
 	case LoadDatabaseErrorMsg:
 		b.focus = ErrorView
 	case loadedDatabaseMsg:
 		b.db = msg.db
+		return b, LoadVaultList(b.db)
 	case LoadVaultsErrorMsg:
 		b.focus = ErrorView
 	case LoadedVaultListMsg:
-		b.vaultList.vaults = msg.vaults
+		var cmd tea.Cmd
+		var m tea.Model
+		m, cmd = b.vaultList.Update(msg)
+		b.status = Visible
+		if vm, ok := m.(VaultListModel); ok {
+			b.vaultList = vm
+		}
+		return b, cmd
 	}
 
 	var cmd tea.Cmd
-	// b, cmd = b.Update(msg)
+	b.spinner, cmd = b.spinner.Update(msg)
 	return b, cmd
 }
 
 func (b Board) View() string {
-	return ""
+	if b.status == Visible {
+		return lipgloss.JoinHorizontal(lipgloss.Center, b.vaultList.View())
+	}
+	return b.spinner.View()
 }
 
 func loadDb(v *viper.Viper) tea.Cmd {
-    return func() tea.Msg {
-        db, err := storage.New(v.GetString("storage_path"))
-        if err != nil {
-            return LoadDatabaseErrorMsg{err}
-        }
-        return loadedDatabaseMsg{db: db}
-    }
+	return func() tea.Msg {
+		db, err := storage.New(v.GetString("storage_path"))
+		if err != nil {
+			return LoadDatabaseErrorMsg{err}
+		}
+		return loadedDatabaseMsg{db: db}
+	}
 }
 
 func LoadVaultList(db *storage.Storage) tea.Cmd {
@@ -89,7 +111,7 @@ func LoadVaultList(db *storage.Storage) tea.Cmd {
 			return LoadVaultsErrorMsg{err}
 		}
 		var vaults []kittypass.Vault
-		for _, vaultInfo := range(vaultsMap) {
+		for _, vaultInfo := range vaultsMap {
 			vault := kittypass.NewVault()
 			vault.Db = db
 			vault.Description = vaultInfo["description"]
--- tui/vaultList.go
+++ tui/vaultList.go
@@ -2,23 +2,22 @@ package tui
 
 import (
 	"github.com/charmbracelet/bubbles/list"
-	"github.com/charmbracelet/bubbles/spinner"
-	"github.com/charmbracelet/lipgloss"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/mrtnhwtt/kittypass/internal/kittypass"
 )
 
 var (
 	blockStyle = lipgloss.NewStyle().
-		Border(lipgloss.NormalBorder(), true, true)
+			Border(lipgloss.NormalBorder(), true, true)
 	titleStyle = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#FFFDF5")).
-		Background(lipgloss.Color("#25A065")).
-		Padding(0, 1)
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Background(lipgloss.Color("#25A065")).
+			Padding(0, 1)
 
 	statusMessageStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.AdaptiveColor{Light: "#04B575", Dark: "#04B575"}).
-			Render
+				Foreground(lipgloss.AdaptiveColor{Light: "#04B575", Dark: "#04B575"}).
+				Render
 )
 
 type item struct {
@@ -31,20 +30,16 @@ func (i item) Description() string { return i.description }
 func (i item) FilterValue() string { return i.title }
 
 type VaultListModel struct {
-	status  status
-	spinner spinner.Model
-	list    list.Model
-	vaults  []kittypass.Vault
+	status status
+	list   list.Model
+	vaults []kittypass.Vault
 }
 
 func NewVaultList() VaultListModel {
 	vl := VaultListModel{
 		status: Loading,
 	}
-	vl.spinner = spinner.New()
-	vl.spinner.Spinner = spinner.Line
-	vl.spinner.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	vl.list = list.New(nil, list.NewDefaultDelegate(), 0,0)
+	vl.list = list.New(nil, list.NewDefaultDelegate(), 0, 0)
 	vl.list.Title = "Vaults"
 	return vl
 }
@@ -56,11 +51,27 @@ func (v VaultListModel) Init() tea.Cmd {
 func (v VaultListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case BlockSizeMsg:
-		v.list.SetSize(msg.Width, msg.Height)
+		// v.list.SetSize(msg.Width, msg.Height)
+		v.list.SetWidth(msg.Width)
+		v.list.SetHeight(msg.Height)
+		blockStyle = blockStyle.Width(msg.Width)
+		blockStyle = blockStyle.Height(msg.Height)
+	case LoadedVaultListMsg:
+		v.vaults = msg.vaults
+		var newItems []list.Item
+		for _, vault := range(v.vaults) {
+			newItems = append(newItems, item{title: vault.Name, description: vault.Description})
+		}
+		v.status = Visible
+		return v, v.list.SetItems(newItems)
 	}
-	return nil, nil
+	return v, nil
 }
 
 func (v VaultListModel) View() string {
-	return blockStyle.Render(v.list.View())
+	if v.status == Visible {
+		return blockStyle.Render(v.list.View())
+	}
+	return "invalid status"
 }
+
